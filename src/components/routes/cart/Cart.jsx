fc51aceb0e6f2016adbc43c86b8ef243aa7c21e9
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-11-30 21:27:09 +0200

    start with form

--- src/components/routes/cart/Cart.jsx
+++ src/components/routes/cart/Cart.jsx
@@ -3,6 +3,10 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 // import { useSelector, useDispatch } from "react-redux";
 // import { resetCart } from "../../../redux/actions/cartActions";
+import {
+  Form, Field, ErrorMessage, Formik,
+} from "formik";
+import { object, string } from "yup";
 import CartItem from "./CartItem";
 import { FormButton } from "../../button/Button";
 import { NEW_CART_URL } from "../../../endpoints/endpoints";
@@ -29,6 +33,7 @@ function Cart() {
   const isCartEmpty = cartItems.length === 0;
   const dispatch = useDispatch();
   const isUserLoggedIn = localStorage.getItem("userLogin") || null;
+  const [openForm, setOpenForm] = useState(false);
 
   // window
   const [showLoginModal, setShowLoginModal] = useState(false);
@@ -56,7 +61,13 @@ function Cart() {
     }
   }
 
-  const handlePurchase = async () => {
+  // const handlePurchaseWithoutLogIn = () => console.log("!");
+
+  const showForm = () => {
+    setOpenForm(true);
+  };
+
+  const handlePurchase = async (region, city, address, postal) => {
     // dispatch(openModal());
     try {
       const cartData = await getCartFromServer();
@@ -64,6 +75,12 @@ function Cart() {
         const { email, telephone, _id: customerId } = cartData.customerId;
         const newOrder = {
           customerId,
+          deliveryAddress: {
+            region,
+            city,
+            address,
+            postal,
+          },
           canceled: false,
           email,
           mobile: telephone,
@@ -91,7 +108,20 @@ function Cart() {
     }
   };
 
-  // const handlePurchaseWithoutLogIn = () => console.log("!");
+  const validationSchema = object().shape({
+    region: string()
+      .required("Поле області адреси доставлення є обов'язковим для заповнення")
+      .matches(/[a-zA-Zа-яА-ЯіІїЇ'єЄ]/, "Дозволені символи для ім'я a-z, A-Z, а-я, А-Я"),
+    city: string()
+      .required("Поле населеного пункту є обов'язковим для заповнення")
+      .matches(/[a-zA-Zа-яА-ЯіІїЇ'єЄ]/, "Дозволені символи для прізвища a-z, A-Z, а-я, А-Я"),
+    address: string()
+      .required("Поле адреси є обов'язковим для заповнення")
+      .matches(/[a-zA-Zа-яА-ЯіІїЇ'єЄ]/, "Дозволені символи для прізвища a-z, A-Z, а-я, А-Я"),
+    postal: string()
+      .required("Поле поштового індекса є обов'язковим для заповнення")
+      .matches(/[0-9]/, "Дозволені символи для пароля: 0-9"),
+  });
 
   return (
     <div className={styles.cardsSectionWrapper}>
@@ -100,27 +130,113 @@ function Cart() {
 
       {isCartEmpty ? <p className={styles.cartEmpty}>Ваш кошик порожній</p>
         : (
-          <table className={styles.cardsListWrapper}>
-            <thead>
-              <tr className={styles.tableRow}>
-                <th>Продукти</th>
-                <th>Ціна</th>
-                <th>Кількість</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cartItems.map((item) => (
-                // eslint-disable-next-line no-underscore-dangle
-                <CartItem key={item._id} item={item} />
-              ))}
-            </tbody>
-          </table>
+          <>
+            <table className={styles.cardsListWrapper}>
+              <thead>
+                <tr className={styles.tableRow}>
+                  <th>Продукти</th>
+                  <th>Ціна</th>
+                  <th>Кількість</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  // eslint-disable-next-line no-underscore-dangle
+                  <CartItem key={item._id} item={item} />
+                ))}
+              </tbody>
+            </table>
+            <FormButton
+              text="Оформити замовлення"
+              padding="10px"
+              onClick={showForm}
+              className={openForm ? styles.hidden : styles.buttonStyle}
+            />
+          </>
         )}
-      {/* {isCartEmpty ? null : <FormButton text="Оформити замовлення"
-      padding="10px" onClick={isUserLoggedIn ? handlePurchase : handlePurchaseWithoutLogIn} />} */}
-      <div className={!showLoginModal ? styles.cardItemIcons : styles.showLoginModal}>
-        {isCartEmpty ? null : <FormButton text="Оформити замовлення" padding="10px" onClick={isUserLoggedIn ? handlePurchase : promptLogin} />}
+      <div className={openForm ? styles.formWrapper : styles.hidden}>
+        <h1 className={styles.headline}>Замовлення</h1>
+        <p className={`${styles.text} ${styles.headlineText}`}> Заповніть форму замовлення</p>
+        <Formik
+          initialValues={{
+            region: "",
+            city: "",
+            address: "",
+            postal: "",
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            handlePurchase(values.region, values.city, values.address, values.postal);
+            setSubmitting(false);
+          }}
+          validationSchema={validationSchema}
+        >
+
+          {({ isSubmitting }) => (
+            <Form className={styles.form}>
+
+              <Field name="region">
+                {({ field, meta }) => (
+                  <input
+                    {...field}
+                    id="region"
+                    className={meta.touched && meta.error ? styles.inputAttention : styles.input}
+                    placeholder="Область"
+                  />
+                )}
+              </Field>
+              <Field name="city">
+                {({ field, meta }) => (
+                  <input
+                    {...field}
+                    id="city"
+                    className={meta.touched && meta.error ? styles.inputAttention : styles.input}
+                    placeholder="Населений пункт"
+                  />
+                )}
+              </Field>
+              <Field name="address">
+                {({ field, meta }) => (
+                  <input
+                    {...field}
+                    id="address"
+                    className={meta.touched && meta.error ? styles.inputAttention : styles.input}
+                    placeholder="Адреса (вулиця, будинок, квартира)"
+                  />
+                )}
+              </Field>
+              <Field name="postal">
+                {({ field, meta }) => (
+                  <input
+                    {...field}
+                    id="postal"
+                    className={meta.touched && meta.error ? styles.inputAttention : styles.input}
+                    placeholder="Поштовий індекс"
+                  />
+                )}
+              </Field>
+
+              <div className={styles.errorsWrapper}>
+                <ErrorMessage name="region" component="p" className={styles.textAttention} />
+                <ErrorMessage name="city" component="p" className={styles.textAttention} />
+                <ErrorMessage name="address" component="p" className={styles.textAttention} />
+                <ErrorMessage name="postal" component="p" className={styles.textAttention} />
+              </div>
+
+              <FormButton
+                type="submit"
+                disabled={isSubmitting}
+                text="Придбати"
+                padding="10px"
+                onClick={isUserLoggedIn ? handlePurchase : promptLogin}
+              />
+            </Form>
+          )}
+        </Formik>
       </div>
+      {/* <div className={!showLoginModal ? styles.cardItemIcons : styles.showLoginModal}> */}
+      {/* eslint-disable-next-line max-len */}
+      {/* {isCartEmpty ? null : <FormButton text="Оформити замовлення" padding="10px" onClick={isUserLoggedIn ? handlePurchase : promptLogin} />}
+      </div> */}
       { showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} /> }
     </div>
   );
